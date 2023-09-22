ae805ba056fca315a151e3290fc36b8e0ab5584d
William Oliveira de Lagos <william.lagos@icloud.com>
2023-09-22 04:12:04 +0200

    feat: started endpoint testing on the backend

--- api/__tests__/index.test.ts
+++ api/__tests__/index.test.ts
@@ -2,9 +2,21 @@ import request from 'supertest'
 
 import app from '../src/'
 
+const API_PREFIX = '/api/v1'
+
 describe('Test app', () => {
   test('Catch-all route', async () => {
-    const res = await request(app).get('/')
+    const res = await request(app).get(`${API_PREFIX}/`)
     expect(res.body).toEqual({ health: 'OK' })
   })
 })
+
+describe('Test /deposit endpoint', () => {
+  test('POST /deposit route', async () => {
+    const userId = '6509a02c607855f89890d034'
+    const res = await request(app).post(`${API_PREFIX}/users/${userId}/deposit`)
+    console.log(res)
+  })
+})
+
+describe('Test /buy endpoint', () => {})
